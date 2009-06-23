b424b7a1925f116604b1ca53a82a621cb1e6c477
Jake Bowers <jwbowers@illinois.edu>
2009-06-23 00:00:00 +0000

    version 0.1-7

--- R/xtable.xbal.R
+++ R/xtable.xbal.R
@@ -1,5 +1,5 @@
 
-xtable.xbal <- function(x,caption = NULL, label = NULL, align =c("l",rep(".",ncol(xvardf))),
+xtable.xbal <- function(x,caption = NULL, label = NULL, align =c("l",rep("r",ncol(xvardf))),
                           digits = 2, display = NULL, col.labels=NULL, ...)
   {##By default use decimal alignment, which will require the dcolumn package in latex and an appropriate column definition like:
     ##\newcolumntype{.}{D{.}{.}{2.2}}
@@ -20,13 +20,25 @@ xtable.xbal <- function(x,caption = NULL, label = NULL, align =c("l",rep(".",nco
     ##Setup the variable by varible table as a dataframe rather than ftable
     xvardf<-data.frame(xprint$vartable[,])
     row.names(xvardf)<-attr(xprint$vartable,"row.vars")[["vars"]]
-    names(xvardf)<-if(!is.null(col.labels)){col.labels} else {
+    names(xvardf) < 
+    
+    if(!is.null(col.labels)) {
+      names(xvardf) <- col.labels
+    } else {
       ##paste(rep(attr(x$vartable,"col.vars")[["strata"]],each=length(attr(x$vartable,"col.vars")[["stat"]])),attr(x$vartable,"col.vars")[["stat"]],sep=".")
-      paste("\\multicolumn{1}{c}{",rep(attr(xprint$vartable,"col.vars")[["stat"]],numstrata),"}",sep="")
+      
+      # MF - commented out below as it was just pasing the \\multi... as plain text
+      # the below works for me (e.g. myvar = 0, myvar = 1) better, if very simple
+      # names(xvardf) <- paste(
+      #   "\\multicolumn{1}{c}{",
+      #   rep(attr(xprint$vartable,"col.vars")[["stat"]], numstrata),
+      #   "}",sep="")
+
+      names(xvardf) <- attr(xprint$vartable,"col.vars")[["stat"]]
     }
 
     ##call xtable on the resulting data.frame
-    vartab <- xtable(xvardf,digits=digits,align=align,hline.after=c(0,nrow(xvardf)),...) ##NextMethod("xtable",xvardf)
+    vartab <- xtable(xvardf,caption=caption, label=label, digits=digits,align=align,hline.after=c(0,nrow(xvardf)),...) ##NextMethod("xtable",xvardf)
     structure(vartab, latex.add.to.row=list(pos=list(-1),command=latex.annotation)) #pos=nrow(vartab),command=latex.annotation))
 
   }
